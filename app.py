58116944c137cc2f9c0cf050ab09b8b9a1cd2770
RDCAZIN <anselmoronald2017@gmail.com>
2026-04-22 21:28:07 -0400

    feat: criação do banco de dados e auntenticacao de login e cadastro

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from routes.main import main
+from models import db  
 
 app = Flask(__name__)
+
+#configuração do banco
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = 'sua_chave_secreta'
+
+db.init_app(app)
+
+#importando os models
+from models.usuarios import Usuario
+
+with app.app_context():
+    db.create_all()
 
 # registrando o blueprint
 app.register_blueprint(main)
