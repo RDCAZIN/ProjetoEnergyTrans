299cfad8d254637dbc6533326a01a43a505ff733
RDCAZIN <anselmoronald2017@gmail.com>
2026-05-19 22:33:21 -0400

    feat: criacao da funcionalidade de cadastro de pontos de coleta

--- models/ponto_coleta.py
+++ models/ponto_coleta.py
@@ -8,8 +8,8 @@ class PontoColeta(db.Model):
     endereco = db.Column(db.String(200), nullable = False)
     materiais_aceitos = db.Column(db.String(300),nullable = False )
     horario_funcionamento = db.Column(db.String(100))
-    latitude = db.Column(db.String(50))
-    longitude = db.Column(db.String(50))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
     aprovado = db.Column(db.Boolean, default=True)
     coletor_id = db.Column(
         db.Integer,
